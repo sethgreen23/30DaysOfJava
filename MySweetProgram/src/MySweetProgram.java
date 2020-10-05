1ca7da558e9aa5d1e2e90dfa4645b188b4c64114
sethgreen23 <dridi.chaith@gmail.com>
2020-10-05 23:51:38 +0100

    work with scanner and files

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,9 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		// Working with files
+		//creating a scanner reading from text file
+		Scanner in = new Scanner(new File("in.text"));
+		//create list to hold the names of students
+		List<String> students = new ArrayList<String>();
+		//iterate throw the file lines and add each line to the list
+		while(in.hasNext()) {
+			students.add(in.nextLine());
+		}
+		//iterate throw the list and output the elements
+		int i=0;
+		for(String s : students) {
+			System.out.println(i+++" : "+s);
+		}
 	}
 
 }
