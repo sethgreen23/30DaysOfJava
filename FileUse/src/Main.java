f61247d4098c75ee51fc665b61b7a8d531a22042
sethgreen23 <dridi.chaith@gmail.com>
2020-10-06 10:28:44 +0100

    use LinkedList

--- FileUse/src/Main.java
+++ FileUse/src/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MySweetProgram {
+public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// Working with files
